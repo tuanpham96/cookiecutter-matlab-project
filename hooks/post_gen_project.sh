939e21cf49f1f4dd2ee5f2f2f31a464ded86d127
Tuan Pham <tuanph18@uchicago.edu>
2020-10-14 16:45:02 -0500

    Fix post_gen_project.sh hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-PROJ_DIR='{{ cookiecutter.project_name }}'
 EXTPKG_DIR='extpkg'
 
-echo pwd
-
-cd $PROJ_DIR
 cd $EXTPKG_DIR
 
 # Add matlab-utils as a submodule
 git submodule add https://github.com/tuanpham96/penguin-matlab-utils
-cp penguin-matlab-utils/example_start_up.m $PROJ_DIR/start_up.m
+cp penguin-matlab-utils/example_start_up.m ../start_up.m
 
 # Clone export_fig
 git clone https://github.com/altmany/export_fig
@@ -22,5 +18,3 @@ git clone https://github.com/DylanMuir/ParforProgMon
 # Clone plotting extras
 git clone https://github.com/raacampbell/shadedErrorBar
 git clone https://github.com/bastibe/Violinplot-Matlab
-
-cd $PROJ_DIR
